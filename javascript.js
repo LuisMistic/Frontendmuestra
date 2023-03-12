dcb7d8b3d8e40f44a364e4c178148f847b7ae5c7
Luis Martinez <luis.escalada@hotmail.com>
2023-03-12 13:21:29 -0300

    armando diseño

---
+++ javascript.js
@@ -0,0 +1,31 @@
+
+document.getElementById('boton').onclick = function () {
+
+
+    document.getElementById("sobremi").innerHTML = " Me considero una persona flexible, con alta capacidad de  adaptabilidad y aprendizaje. Actualmente me encuentro trabajando en KOMO en la digitalizacion y el ingreso de datos como tambien desarrllando contenido audiovisual en la compania de Teatro, MLM (Mi lugar en el Mundo) ";
+}
+
+
+
+document.getElementById('boton1').onclick = function () {
+
+
+    document.getElementById("sobremi").innerHTML = " La sección de experiencia es una parte indispensable en cualquier CV. Aún si eres muy joven, estás estás estudiando o llevas poco tiempo trabajando, tu currículum debe de incluir una experiencia laboral capaz de cautivar la atención del reclutador.";
+}
+
+
+document.getElementById('boton2').onclick = function () {
+
+
+    document.getElementById("sobremi").innerHTML = " La educación es el proceso de facilitar el refinamiento de habilidades o capacidades propias del individuo, mediante el aprendizaje o la construcción de conocimientos, así como también de las virtudes, creencias, hábitos, u otras características del ser..";
+}
+
+
+document.getElementById('boton3').onclick = function () {
+
+
+    document.getElementById("sobremi").innerHTML = " En el sentido más amplio, las habilidades son un conjunto de capacidades, competencias, atributos, talentos, y en algunos casos conocimientos, que caracterizan a las personas. Esta es la definición consensuada que maneja el Banco Interamericano de Desarrollo en la actualidad.";
+}
+
+
+
